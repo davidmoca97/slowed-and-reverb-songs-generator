dfee3e52f626ca82b6bda219d76dd527e385cd3c
davidmoca97 <davidmoca97@gmail.com>
2020-06-27 17:45:21 -0500

    pre delay in reverb

--- src/MainView/Controls/Controls.tsx
+++ src/MainView/Controls/Controls.tsx
@@ -4,16 +4,18 @@ export interface IControlsProps {
     playbackRate: number;
     reverbWet: number;
     reverbDecay: number;
+    reverbPreDelay: number;
     onPlayBackRateChange: (value: number) => void;
     onReverbWetChange: (value: number) => void;
     onReverbDecayChange: (value: number) => void;
+    onReverbPreDelayChange: (value: number) => void;
     disabled: boolean;
 }
 
 export const Controls: React.FC<IControlsProps> =
     ({ 
-        onPlayBackRateChange, onReverbWetChange, onReverbDecayChange, 
-        playbackRate, reverbWet, reverbDecay,
+        onPlayBackRateChange, onReverbWetChange, onReverbDecayChange, onReverbPreDelayChange,
+        playbackRate, reverbWet, reverbDecay, reverbPreDelay,
         disabled 
     }) => {
         return (
@@ -50,15 +52,28 @@ export const Controls: React.FC<IControlsProps> =
                 <div style={{ display: 'flex' }}>
                     <pre>Decay</pre>&nbsp;
                      <input
-                        disabled={disabled}
+                        // disabled={disabled}
                         type="range"
-                        min="0.1"
+                        min="0.5"
                         max="10"
                         step="0.5"
                         value={reverbDecay}
-                        onChange={(e) => { console.log(e.type); onReverbDecayChange(Number(e.target.value))}}
+                        onChange={(e) => onReverbDecayChange(Number(e.target.value))}
                     />&nbsp;&nbsp;
                     <pre>{reverbDecay}</pre>
+                </div>
+                <div style={{ display: 'flex' }}>
+                    <pre>Pre-delay</pre>&nbsp;
+                     <input
+                        disabled={disabled}
+                        type="range"
+                        min="0"
+                        max="1"
+                        step="0.1"
+                        value={reverbPreDelay}
+                        onChange={(e) => onReverbPreDelayChange(Number(e.target.value))}
+                    />&nbsp;&nbsp;
+                    <pre>{reverbPreDelay}</pre>
                 </div>
             </div>
         )
--- src/MainView/MainView.tsx
+++ src/MainView/MainView.tsx
@@ -2,13 +2,20 @@ import React, { useState, useEffect, useMemo } from 'react';
 import * as Tone from "tone";
 import { Player } from './Player/Player';
 import { Controls } from './Controls/Controls';
+import debounce from 'lodash.debounce';
+
+function _updateReverbDecay(reverb: Tone.Reverb, value: number) { reverb.decay = value };
+function _updateReverbPreDelay(reverb: Tone.Reverb, value: number) { reverb.preDelay = value };
+const updateReverbDecay = debounce(_updateReverbDecay, 500);
+const updateReverbPreDelay = debounce(_updateReverbPreDelay, 500);
 
 const TRACK_URL = `${process.env.PUBLIC_URL}/track.mp3`;
 
 const defaultValues = {
     playBackRate: 0.8,
     reverbWet: 0.5,
-    reverbDecay: 0.5
+    reverbDecay: 5,
+    reverbPreDelay: 0.1
 };
 
 interface MainViewProps { }
@@ -18,10 +25,12 @@ export const MainView: React.FC<MainViewProps> = () => {
     const [loading, setLoading] = useState<boolean>(true);
     const [songInfo, setSongInfo] = useState({ length: 0 });
     const [currentPlayback, setCurrentPlayback] = useState<number>(0);
+    const [isPlaying, setIsPlaying] = useState(false);
 
     const [playbackRate, setPlaybackRate] = useState<number>(defaultValues.playBackRate);
     const [reverbWet, setReverbWet] = useState<number>(defaultValues.reverbWet);
     const [reverbDecay, setReverbDecay] = useState<number>(defaultValues.reverbDecay);
+    const [reverbPreDelay, setReverbPreDelay] = useState<number>(defaultValues.reverbPreDelay);
 
     const reverb = useMemo(() => new Tone.Reverb(), []);
     const player = useMemo(() => new Tone.Player(), []);
@@ -38,6 +47,7 @@ export const MainView: React.FC<MainViewProps> = () => {
         Promise.all([p1, p2]).then(() => {
             player.playbackRate = defaultValues.playBackRate;
             reverb.wet.value = defaultValues.reverbWet;
+            reverb.decay = defaultValues.reverbDecay;
             reverb.toDestination();
             player.connect(reverb);
             onLoading();
@@ -58,7 +68,6 @@ export const MainView: React.FC<MainViewProps> = () => {
         player.start(0, value);
         setCurrentPlayback(value);
     };
-    const [isPlaying, setIsPlaying] = useState(false);
 
     useEffect(() => {
         // if (isPlaying) {
@@ -78,15 +87,17 @@ export const MainView: React.FC<MainViewProps> = () => {
         player.playbackRate = value;
         setPlaybackRate(value);
     }
-
     const onReverbWetChange = (value: number) => {
         reverb.wet.value = value;
         setReverbWet(value);
     }
-
     const onReverbDecayChange = (value: number) => {
-        reverb.decay = value;
+        updateReverbDecay(reverb, value);
         setReverbDecay(value);
+    }
+    const onReverbPreDelayChange = (value: number) => {
+        updateReverbPreDelay(reverb, value);
+        setReverbPreDelay(value);
     }
 
     if (loading) {
@@ -113,9 +124,11 @@ export const MainView: React.FC<MainViewProps> = () => {
                         playbackRate={playbackRate}
                         reverbWet={reverbWet}
                         reverbDecay={reverbDecay}
+                        reverbPreDelay={reverbPreDelay}
                         onPlayBackRateChange={onPlayBackRateChange}
                         onReverbWetChange={onReverbWetChange}
                         onReverbDecayChange={onReverbDecayChange}
+                        onReverbPreDelayChange={onReverbPreDelayChange}
                     />
                 </div>
             </div>
@@ -123,12 +136,3 @@ export const MainView: React.FC<MainViewProps> = () => {
         </div>
     );
 };
-
-    // const buffer = new Tone.Buffer(TRACK_URL, () => {
-    //     const buff = buffer.get();
-    //     console.log(buff);
-    // }, (error) => {
-    //     console.error(error);
-    // });
-    // play as soon as the buffer is loaded
-    // player.autostart = true;
